aea20626abc4a99d0b6f2604cbae566e348dcc72
Muheeb Ali Jamali <160766398+Jamali28@users.noreply.github.com>
2024-12-08 15:37:34 +0500

    initial commit

---
+++ src/app/Component/HeroSec.tsx
@@ -0,0 +1,12 @@
+export default function HeroSec(){
+    return(
+        <div className="w-[1250px] justify-center h-[450px] mt-[1070px] ml-[64px] bg-black">
+           <div className="text-white w-[562px] h-[562px] ">
+            <h1 className="text-yellow-500 w-[91px] font-[Great Vibes] h-[40px]">About us</h1>
+
+           </div>
+
+        
+        </div>
+    )
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import Head from "./Component/Head";
+import HeroSec from "./Component/HeroSec";
+
+
+export default function Home() {
+  return (
+   <div>
+     
+     <Head />
+     <HeroSec />
+   </div>
+  );
+}
